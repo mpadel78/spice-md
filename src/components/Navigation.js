a4e3f632bbc031b97dd4391b37debf38de952305
Manish Paudel <mpaudel78@gmail.com>
2022-09-23 22:30:03 -0500

    Broken down into components

---
+++ src/components/Navigation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navigation() {
+  return <div>Navigation Page</div>;
+}
+
+export default Navigation;
---
+++ src/components/Symptoms.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Symptoms() {
+  return <div>Symptoms Page</div>;
+}
+
+export default Symptoms;
