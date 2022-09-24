aac0f30fa70693a2cad4012f3f6aba45c70143e1
Manish Paudel <mpaudel78@gmail.com>
2022-09-24 12:46:52 -0500

    React router and boiler plate for the components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,80 @@
-import logo from "./logo.svg";
-import "./App.css";
+//import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Symptoms from "./components/Symptoms";
+import Instructions from "./components/Instructions";
+import Recommendation from "./components/Recommendation";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+
+import Grid from "@mui/material/Grid";
 
 function App() {
-  return <div className="App">Here is the application.</div>;
+  return (
+    <BrowserRouter>
+      <Grid container spacing={2}>
+        <Grid
+          item
+          xs={12}
+          md={12}
+          lg={12}
+          style={{
+            textAlign: "center",
+            paddingLeft: "30px",
+            height: "7vh",
+            backgroundColor: "green",
+          }}
+        >
+          <Navigation />
+        </Grid>
+
+        <Grid
+          item
+          xs={12}
+          md={12}
+          lg={12}
+          style={{
+            textAlign: "center",
+            paddingLeft: "30px",
+            height: "86vh",
+            backgroundColor: "grey",
+          }}
+        >
+          <Routes>
+            <Route exact path="/" element={<Welcome />}></Route>
+            <Route exact path="/symptoms" element={<Symptoms />}></Route>
+            <Route
+              exact
+              path="/instructions"
+              element={<Instructions />}
+            ></Route>
+
+            <Route
+              exact
+              path="/recommendation"
+              element={<Recommendation />}
+            ></Route>
+
+            <Route exact path="/navigation" element={<Navigation />}></Route>
+          </Routes>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={12}
+          lg={12}
+          style={{
+            textAlign: "center",
+            paddingLeft: "30px",
+            height: "10vh",
+            backgroundColor: "green",
+          }}
+        >
+          <Footer />
+        </Grid>
+      </Grid>
+    </BrowserRouter>
+  );
 }
 
 export default App;
